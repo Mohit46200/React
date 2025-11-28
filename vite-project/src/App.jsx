9106ad4c8754f3e9302dbbb88ce43e50c9b3024e
Mohit <mohit22600@gmail.com>
2025-11-28 19:15:17 +0530

    counter

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,28 @@
+import { useState } from 'react'
+
 function App() {
+  let [counter,setCounter]=useState(15);
+  function add(){
+    counter=counter+1
+    setCounter(counter)
+    if(counter>20){
+      setCounter(counter=20)
+    }
+  }
+  function remove(){
+    
+    setCounter(counter=counter-1)
+    if(counter<0){
+      setCounter(counter=0)
+    }
+  }
+
   return (
-    <h1>Mohit</h1>
+  <>
+    <h1>Counter value {counter}</h1>
+    <button onClick={add}>Add value</button>
+    <button onClick={remove}>Remove value</button>
+  </>
   )
 }
 export default App
