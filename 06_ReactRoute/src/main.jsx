3c876da01762a7da3f0d1fe1a8202b19049d7082
Mohit <mohit22600@gmail.com>
2025-12-12 16:26:11 +0530

    added more content

--- 06_ReactRoute/src/main.jsx
+++ 06_ReactRoute/src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/layout.jsx'
 import Home from './components/home.jsx'
+import About from './components/about.jsx'
+import Contact from './components/contactus.jsx'
 
 const router=createBrowserRouter([
   {
@@ -15,6 +16,14 @@ const router=createBrowserRouter([
       {
         path: '',
         element: <Home/>
+      },
+      {
+        path: '/about',
+        element: <About/>
+      },
+      {
+        path:'/contact_us',
+        element:<Contact/>
       }
     ]
   }
