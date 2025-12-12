0debc2d4486a84b44b3ce4f6ec8163dc5f18edcc
Mohit <mohit22600@gmail.com>
2025-12-12 16:04:28 +0530

    Route

--- 06_ReactRoute/src/App.jsx
+++ 06_ReactRoute/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
-
+import Header from './components/header'
+import Footer from './components/footer'
+import Home from './components/home'
 
 function App() {
-  
+
+
   return (
     <>
-      
+     
     </>
   )
 }
--- 06_ReactRoute/src/main.jsx
+++ 06_ReactRoute/src/main.jsx
@@ -1,10 +1,28 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/layout.jsx'
+import Home from './components/home.jsx'
+
+const router=createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children:[
+      {
+        path: '',
+        element: <Home/>
+      }
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
 )
