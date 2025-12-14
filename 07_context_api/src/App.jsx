47841ad8f8c3fdfd537bbc51814ae12931a59bfc
Mohit <mohit22600@gmail.com>
2025-12-14 10:51:15 +0530

    contextApi

---
+++ 07_context_api/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/login'
+import Profile from './components/profile'
+function App() {
+    
+
+  return (
+    <UserContextProvider>
+      <h1>React context api</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ 07_context_api/src/components/login.jsx
@@ -0,0 +1,36 @@
+import React,{useState,useContext} from "react";
+import userContext from "../context/UserContext";
+function login(){
+    const [username,setUsername]=useState('')
+    const [password,setPassword]=useState('')
+    
+    const {setUser} = useContext(userContext)
+
+    
+    const handleSubmit =function(e){
+        e.preventDefault()
+        setUser({username,password})
+    }
+    return (
+        <div>
+
+            <h2>Login</h2>
+            <input type="text" 
+            value={username} 
+            onChange={function(e){
+                setUsername(e.target.value)
+            }}
+            placeholder="username"></input>
+            <input type="text" 
+            value={password} 
+            onChange={function(e){
+                setPassword(e.target.value)
+            }}
+            placeholder="password"></input>
+            <button onClick={handleSubmit}>Submit</button>
+
+        </div>
+    )
+}
+
+export default login
