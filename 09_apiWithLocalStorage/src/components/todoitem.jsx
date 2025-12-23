4ff3008218ea1bf8770b8988daeca5b283f97186
Mohit <mohit22600@gmail.com>
2025-12-23 13:52:15 +0530

    todo finally completed

--- 09_apiWithLocalStorage/src/components/todoitem.jsx
+++ 09_apiWithLocalStorage/src/components/todoitem.jsx
@@ -4,13 +4,18 @@ function TodoItem({ todo }) {
     const [isTodoEditable,setIsTodoEditable] = useState(false)
     const [todoMsg,setTodoMsg] = useState(todo.todo)
     const {updateTodo,deleteTodo,toggleComplete} = UseTodo()
+
     const editTodo = function(){
         updateTodo(todo.id , {...todo,todo:todoMsg})
         setIsTodoEditable(false)
     }
+
     const toggleCompleted = function(){
         toggleComplete(todo.id)
     }
+
+
+    
     return (
         <div
             className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
