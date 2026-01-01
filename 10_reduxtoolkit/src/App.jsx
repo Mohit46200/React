1f8708485ed148244637a48883505c3913fbb5a0
Mohit <mohit22600@gmail.com>
2026-01-01 13:06:14 +0530

    redux toolkit

--- 10_reduxtoolkit/src/App.jsx
+++ 10_reduxtoolkit/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
-import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
 
 function App() {
   
-    return (
-        <>
-          
-        </>
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo/>
+      <Todos/>
 
-    )
-  
+    </>
+  )
 }
 
 export default App
